6c59c6a209769b9aa1a030503ae0604eeccdc130
Team603 <Team603@LAPTOP-A60DFD8I>
2021-01-11 17:10:57 +0900

    upload

---
+++ BlazorApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*string jsonPath = @"C:\Users\Team603\Desktop\test.json";
+            string json = File.ReadAllText(jsonPath);
+            JObject jObject = JObject.Parse(json);
+
+            var a = from x in jObject["data"]
+                    select x.ToList();
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item[0]);
+                //Console.WriteLine(item[0]["id"]);
+            }*/
+
+
+            string jsonPath = @"C:\Users\Team603\Desktop\csharp\BlazorApp1\BlazorApp1\championFull.json";
+            string json = File.ReadAllText(jsonPath);
+            JObject jObject = JObject.Parse(json);
+
+            var a = from x in jObject["data"]
+                    select x.ToList();
+
+            List<List<JToken>> tt = new List<List<JToken>>();
+
+            foreach (var item in a)
+            {
+                tt.Add(item);
+            }
+
+            Console.WriteLine(tt[0][0].ToString());
+
+        }
+    }
+}
