2019e464ba0426899cc039ef5038b84e0a0c4828
YangChanghyeok <ych_iu@naver.com>
2021-01-12 14:40:58 +0900

    update

--- BlazorApp1/BlazorApp1/obj/Release/netcoreapp3.1/RazorDeclaration/App.razor.g.cs
+++ BlazorApp1/BlazorApp1/obj/Release/netcoreapp3.1/RazorDeclaration/App.razor.g.cs
@@ -13,63 +13,63 @@ namespace BlazorApp1
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 1 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 2 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 3 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 4 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 5 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 6 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 7 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 8 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using BlazorApp1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Team603\Desktop\Personal\BlazorApp1\BlazorApp1\_Imports.razor"
+#line 9 "C:\Users\Yang_CH\Desktop\Project\Project3\BlazorApp1\BlazorApp1\_Imports.razor"
 using BlazorApp1.Shared;
 
 #line default
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# PersonalProject
-Personal
+# GameInfo(v1.0.0) : 
+
+# 개요
+- 게임 정보 및 전적 검색 사이트 구현
+- 일반 사이트를 블레이저 사이트로 구현 예정
+
+# 개발 기간
+- 2021년 01월 11일 ~ 진행중
+
+# 구성원
+- 양창혁
+
+# 기능 목록
+
+## GameInfo Site
+
+### 1. 전적 검색 화면
+- 진행중
+
+### 2. LOL info 화면
+
+<div>
+<img src=".\BlazorApp1\BlazorApp1\Images\lolInfo.PNG" width="100%">
+</div>
+
+- lol 챔피언의 정보를 나타냈다.
+- skill의 사진을 누르면 skill video에서 누른 스킬영상이 나온다
+- 버튼을 누르면 다음 챔피언을 볼 수 있다
+
+### 3. Overwatch info 화면
+- 진행중
+
+
+# 사용 기술
+
+## 언어
+
+- C# 8.0
+- HTML
+
+## 프레임워크
+
+- ASP.Net Core
+
+## 데이터베이스
+
+* 미사용
+
+## 기타 개발환경
+
+- Windows 10
+- Microsoft Visual Studio Community 2019 v16.8
+- IIS(인터넷 정보 서비스)
+
+# 데이터베이스 스키마
+
+* 미사용
+ 
+# UML
+
+## 클래스 다이어그램
+- 진행중
+
+## User Case Diagram
+- 진행중
+
+## 순서도
+- 진행중
+
+# Point of Interest
+- 진행중
+
